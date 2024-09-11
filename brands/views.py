ee909b0428d1839a2845f953f6cd9edd44f68d43
Paulo Vitor <paulovitorcarneiro11@hotmail.com>
2024-09-11 00:21:59 -0300

    add brand_list route and template for listing brands

--- brands/views.py
+++ brands/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from typing import Any
+from django.db.models.query import QuerySet
+from django.views.generic import ListView
+from . import models
 
-# Create your views here.
+
+class BrandListView(ListView):
+    model = models.Brand
+    template_name = 'brand_list.html'
+    context_object_name = 'brands'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        name = self.request.GET.get('name')
+
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+
+        return queryset
