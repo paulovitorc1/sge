667f500541aa72619c34d92cb9e6d582dd0ca99f
Paulo Vitor <paulovitorcarneiro11@hotmail.com>
2024-12-17 23:26:01 -0300

    Update Inflows and Outflows

--- outflow/apps.py
+++ outflow/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class OutflowConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "outflow"
+
+    def ready(self):
+        import outflow.signals
