34a1a9df1a30cc411c4b26e14c6a36b2e9c3dc4e
Paulo Vitor <paulovitorcarneiro11@hotmail.com>
2025-01-17 12:17:02 -0300

    Dashboard created and authentication process implemented.

--- app/settings.py
+++ app/settings.py
@@ -46,6 +46,11 @@ INSTALLED_APPS = [
     'outflow',
 ]
 
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/login/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -110,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
